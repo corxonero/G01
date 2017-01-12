bead360834e465145991c790c5bcb62c738c6bba
corxonero <corxonero@gmail.com>
2017-01-12 18:33:27 +0100

    sum y resta de 3 numeros

--- src/main/Resta.java
+++ src/main/Resta.java
@@ -5,4 +5,8 @@ public class Resta {
 	public static int res(int a, int b) {
 		return a - b;
 	}
+	
+	public static int res(int a, int b, int c) {
+		return a - b - c;
+	}
 }
--- src/main/Sum.java
+++ src/main/Sum.java
@@ -5,5 +5,9 @@ public class Sum {
 	public static int sum(int a, int b) {
 		return a + b;
 	}
+	
+	public static int sum(int a, int b, int c) {
+		return a + b + c;
+	}
 
 }

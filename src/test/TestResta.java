dabab05d104f9ae8450c0754db994c149fd9c3a4
corxonero <corxonero@gmail.com>
2017-01-12 18:27:10 +0100

    añadido test Resta

---
+++ src/test/TestResta.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Resta;
+
+public class TestResta {
+
+	@Test
+	public void testRes2() {
+		assertEquals(0, Resta.res(2, 2));
+	}
+
+}
